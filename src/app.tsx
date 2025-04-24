1019efca7c9fe871f86717abab4326be2973c21d
Alvi19 <mohalviridho@gmail.com>
2025-04-24 11:49:35 +0700

    first commit

---
+++ src/app.tsx
@@ -0,0 +1,103 @@
+import * as React from "react";
+import ReactDOM from "react-dom/client";
+import { connect, Provider } from "react-redux";
+import AutoSizer from "react-virtualized/dist/commonjs/AutoSizer";
+import { applyMiddleware, combineReducers, compose, createStore } from "redux";
+import document from "global/document";
+import keplerGlReducer, { enhanceReduxMiddleware } from "@kepler.gl/reducers";
+import KeplerGl from "@kepler.gl/components";
+import { addDataToMap } from "@kepler.gl/actions";
+import { KeplerGlSchema } from "@kepler.gl/schemas";
+
+// Reducers & Store
+const reducers = combineReducers({
+  keplerGl: keplerGlReducer.initialState({
+    uiState: {
+      readOnly: false,
+      currentModal: null,
+    },
+  }),
+});
+
+const middleWares = enhanceReduxMiddleware([
+  /* Add other middlewares here */
+]);
+const enhancers = applyMiddleware(...middleWares);
+const initialState = {};
+const store = createStore(reducers, initialState, compose(enhancers));
+
+const App = ({ dispatch }) => {
+  React.useEffect(() => {
+    fetch("/kepler.gl.json")
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error("File not found or fetch error");
+        }
+        return res.json();
+      })
+      .then((json) => {
+        console.log("✅ JSON Loaded:", json);
+        const loaded = KeplerGlSchema.load(json);
+        console.log("🔄 Parsed Schema:", loaded);
+        dispatch(addDataToMap(loaded));
+      })
+      .catch((err) => {
+        console.error("❌ Error loading JSON:", err);
+      });
+
+    fetch("/layer2-data.json")
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error("File not found or fetch error 2");
+        }
+        return res.json();
+      })
+      .then((json) => {
+        console.log("✅ JSON Loaded: kepler2.json", json);
+        const loaded = KeplerGlSchema.load(json);
+        console.log("🔄 Parsed Schema: kepler2.json", loaded);
+        dispatch(addDataToMap(loaded));
+      })
+      .catch((err) => {
+        console.error("❌ Error loading kepler2.json:", err);
+      });
+  }, [dispatch]);
+
+  return (
+    <div
+      style={{
+        position: "absolute",
+        top: "0px",
+        left: "0px",
+        width: "100%",
+        height: "100%",
+      }}
+    >
+      <AutoSizer>
+        {({ height, width }) => (
+          <KeplerGl
+            mapboxApiAccessToken="pk.eyJ1Ijoic3VwYXJkaTk4IiwiYSI6ImNtOXF5aGZrMDFqamYyanM5ZmhxZ2JsbHgifQ.eFm0ugCgcGv9hOSFYSFvkA"
+            id="map"
+            width={width}
+            height={height}
+          />
+        )}
+      </AutoSizer>
+    </div>
+  );
+};
+
+// Connect App to Redux
+const mapStateToProps = (state) => state;
+const dispatchToProps = (dispatch) => ({ dispatch });
+const ConnectedApp = connect(mapStateToProps, dispatchToProps)(App);
+
+const Root = () => (
+  <Provider store={store}>
+    <ConnectedApp />
+  </Provider>
+);
+
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+root.render(<Root />);
